efe51be2f1c5d3cbc3dda92704bdc582144688c3
mbilal.amjad@outlook.com <mbilal.amjad@outlook.com>
2025-04-07 11:34:52 -0700

    Add terraform.tf [skip ci]

---
+++ terraform.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    alz = {
+      source  = "Azure/alz"
+      version = "~> 0.16"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "alz" {
+  library_overwrite_enabled = true
+  library_references = [
+    {
+      custom_url = "${path.root}/lib"
+    }
+  ]
+}
+
+provider "azapi" {
+  skip_provider_registration = true
+  subscription_id            = var.subscription_id_management
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  alias                           = "management"
+  subscription_id                 = var.subscription_id_management
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  alias                           = "connectivity"
+  subscription_id                 = var.subscription_id_connectivity
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
