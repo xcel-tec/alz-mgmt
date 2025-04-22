e0fb702e204e9ffc3d6029eb2fea53d70ef7d519
mbilal.amjad@outlook.com <mbilal.amjad@outlook.com>
2025-04-21 23:28:41 -0700

    Add main.connectivity.virtual.wan.tf [skip ci]

---
+++ main.connectivity.virtual.wan.tf
@@ -0,0 +1,15 @@
+module "virtual_wan" {
+  source  = "Azure/avm-ptn-alz-connectivity-virtual-wan/azurerm"
+  version = "0.2.0"
+
+  count = local.connectivity_virtual_wan_enabled ? 1 : 0
+
+  virtual_wan_settings = local.virtual_wan_settings
+  virtual_hubs         = local.virtual_wan_virtual_hubs
+  enable_telemetry     = var.enable_telemetry
+  tags                 = module.config.tags
+
+  providers = {
+    azurerm = azurerm.connectivity
+  }
+}
